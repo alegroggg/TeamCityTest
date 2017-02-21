a591f423cfb1730916504ec2f71f74d9b5a1c734
Grzegorz G <grze-grze@wp.pl>
2017-02-22 00:05:11 +0100

    Unit Test Project Added

---
+++ ConsoleApplication1/ConsoleApplication1/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Cat
+    {
+        public int Age { get; set; }
+
+        public int ReturnAge()
+        {
+            return this.Age;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CatTestMethod()
+        {
+            Cat cat = new Cat();
+            cat.Age = 20;
+
+            Assert.AreEqual(cat.ReturnAge(), 20);
+        }
+    }
+}
